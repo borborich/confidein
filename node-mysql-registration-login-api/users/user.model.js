0d6c6d6b731461d0359ba50386d5b5e98336e697
borborich <borborich@bk.ru>
2022-01-12 11:51:13 +0100

    node mysql registration login api

---
+++ node-mysql-registration-login-api/users/user.model.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        firstName: { type: DataTypes.STRING, allowNull: true },
+        lastName: { type: DataTypes.STRING, allowNull: true },
+        username: { type: DataTypes.STRING, allowNull: false },
+        hash: { type: DataTypes.STRING, allowNull: true }
+    };
+
+    const options = {
+        defaultScope: {
+            // exclude hash by default
+            attributes: { exclude: ['hash'] }
+        },
+        scopes: {
+            // include hash with this scope
+            withHash: { attributes: {}, }
+        }
+    };
+
+    return sequelize.define('User', attributes, options);
+}
